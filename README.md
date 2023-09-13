20249048372034640aad30fa624eabca9b9c01b1
Emerson Bezerra <emersonbbezerra@gmail.com>
2023-09-12 22:47:06 -0300

    Creating the components

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularPsnStoreClone
+# Store
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-psn-store-clone'),
+      dir: require('path').join(__dirname, './coverage/store'),
       subdir: '.',
       reporters: [
         { type: 'html' },
